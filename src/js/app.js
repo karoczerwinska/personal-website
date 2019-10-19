9e0bfe655379a8b175f5e08ed6eb4f10dfe8694b
Karolina Czerwińska <karo@macbook-air-karolina-2.home>
2019-10-19 19:58:00 +0200

    welcome message added

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import { intro } from "./intro";
 import { navigation } from "./navigation";
+import { TextTyping } from "./welcome-message"
 
 document.addEventListener('DOMContentLoaded', function() {
     intro();
     navigation();
+    TextTyping;
 });
