d90a456d97e2a50894adb90690565d4d3518c2df
Karolina Czerwińska <karo@macbook-air-karolina-2.home>
2019-10-19 21:27:43 +0200

    intro animations added

--- src/js/intro.js
+++ src/js/intro.js
@@ -4,6 +4,51 @@ function intro() {
         top: '-110%',
         ease: Expo.easeInOut
   });
+    TweenMax.staggerFrom(".social-media ul li", 2, {
+        delay: 8.7,
+        opacity: 0,
+        y: 20,
+        ease: Power3.easeInOut
+    }, 0.1);
+    TweenMax.staggerFrom(".sidebar-menu-container ul li", 2, {
+        delay: 8.7,
+        opacity: 0,
+        y: 20,
+        ease: Power3.easeInOut
+    }, 0.1);
+    TweenMax.from(".menu-icon", 3, {
+        delay: 8.8,
+        opacity: 0,
+        y: 20,
+        ease: Expo.easeInOut
+  });
+  TweenMax.from(".greeting", 3, {
+    delay: 8.2,
+    opacity: 0,
+    y: 20,
+    ease: Power3.easeInOut
+});
+
+TweenMax.from(".name", 3, {
+    delay: 8.5,
+    opacity: 0,
+    y: 20,
+    ease: Power3.easeInOut
+});
+
+TweenMax.from(".position", 3, {
+    delay: 8.8,
+    opacity: 0,
+    y: 20,
+    ease: Power3.easeInOut
+});
+
+TweenMax.from(".phrase", 3, {
+    delay: 9.1,
+    opacity: 0,
+    y: 20,
+    ease: Power3.easeInOut
+});
 
 let timeline = new TimelineMax();
 
